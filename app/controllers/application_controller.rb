64afcf4f2ab887fe255ff0a7b52534ce51267faf
mgomez11 <mgomez@buk.cl>
2022-07-18 01:11:44 -0300

    bug: Typo en permitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:game_uid, :codnickname, :cellphone, :country_code, :time_zone])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:uid, :codnickname, :cellphone, :country_code, :time_zone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:game_uid, :codnickname, :cellphone, :country_code, :time_zone])
   end
 end
