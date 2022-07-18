a5928baa8e6ecea229de981ffa3e94ebdcac6145
mgomez11 <mgomez@buk.cl>
2022-07-18 00:57:08 -0300

    Allow editing without password for google oauth

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -23,6 +23,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def update
   #   super
   # end
+  #
+  def update_resource(resource, params)
+    if current_user.provider == "google_oauth2"
+      params.delete("current_password")
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
+  end
+
 
   # DELETE /resource
   # def destroy
